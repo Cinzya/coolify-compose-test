4c1e5f601150863067574da59a258199837e65fc
Cynthia Ebert <54354036+Cinzya@users.noreply.github.com>
2025-03-22 15:34:58 +0100

    Update serve.js

--- bin/serve.js
+++ bin/serve.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+app.use(express.json())
 
 app.get('/', (req, res) => {
   console.log(`request: ${req.url}`)
-  res.send('Hello World!');
+  res.status(200).send('Hello World');
 });
 
 app.listen(port, '0.0.0.0', () => {
